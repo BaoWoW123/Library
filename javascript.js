23fab1d4404d67ae321793a4750ce8ef03f3e255
BaoWoW <baonguyen9392@gmail.com>
2022-11-04 06:29:30 -0700

    Add Buttons To Book, Limit Text, & Change Grid
    Putted add book button into form and prevented event default.
    Limited text length of title and author for book.
    Changed content div layout to grid.
    Included Read/not Read and remove button onto book div.

--- javascript.js
+++ javascript.js
@@ -1,40 +1,74 @@
 let form = document.querySelector('.addBook');
 let content = document.querySelector('.content');
 
-/* form.addEventListener('click', () => {
-    let form = document.createElement('form');
-    form.className = 'form'
-    form.textContent='Book Info';
-    content.appendChild(form)
-}); */
-
 let library = [];
 /* constructor */
-function book(title, author, pages) {
+function book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.read = read;
     this.info = () => {
-        return `${title},${author},${pages}`
+        return `${author}, ${pages}`
     } 
     
 }
 let theHobbit = new book('Some title', 'me', '123')
 console.log(theHobbit.info())
-function addBook() {
+function addBook(event) {
+    event.preventDefault();
     let title = document.querySelector('.title').value;
     let author = document.querySelector('.author').value;
     let pages = document.querySelector('.pages').value;
-    let bookInfo = new book(title, author, pages)
+    let read = document.querySelector('.read');
+    (read.checked) ? read = 'read': read = 'notRead'; 
+    let bookInfo = new book(title, author, pages, read);
     if (title === '' || author === '') return;
     library.push(bookInfo)
     content.innerHTML = ''
+
     for (i = 0; i < library.length; i++) {
-        /* display array into content */
         let div = document.createElement('div') 
-        div.className = 'book'  
-        div.textContent = library[i].info()
-        content.appendChild(div)
+        div.className = 'book';
+        let divTitle = document.createElement('p')
+        let divAuthor = document.createElement('p')
+        let divPage = document.createElement('p');
+        let readBtn = document.createElement('button')
+        let remove = document.createElement('button')
+
+        divTitle.textContent = trimTitle();
+        divAuthor.textContent = trimAuthor();
+        divPage = `Pages ${library[i].pages}`;
+
+        function trimTitle() {
+        return (library[i].title.length >= 20) ? 
+            library[i].title.substring(0, 17) + '...':
+            library[i].title;
+        }
+        function trimAuthor() {
+            return (library[i].author.length >= 20) ? 
+                library[i].author.substring(0, 17) + '...':
+                library[i].author;
+            }
+
+        if (library[i].read === 'notRead') {
+            readBtn.className = 'readBtn-Not';
+            readBtn.textContent = 'Not Read';
+        }
+        else {
+            readBtn.className = 'readBtn-Read';
+            readBtn.textContent = 'Read';
+        }
+        
+        remove.className = 'remove'
+        remove.textContent = 'X'
+        
+        content.append(div)
+        div.append(divTitle)
+        div.append(divAuthor)
+        div.append(divPage)
+        div.append(readBtn)
+        div.append(remove)
     }
 }
 
