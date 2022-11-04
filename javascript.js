7ea2b7774785295982bf3b9ee598348c11215c82
BaoWoW <baonguyen9392@gmail.com>
2022-11-03 23:01:13 -0700

    Change CSS Layout & Output Form into Content
    Form has own left tab rather than plan of popping up after button click.
    Form info is stored into an object and then library[].
    For loop clears all previous text in content div then loops through array.

--- javascript.js
+++ javascript.js
@@ -1,10 +1,41 @@
-let addBook = document.querySelector('.addBook');
+let form = document.querySelector('.addBook');
 let content = document.querySelector('.content');
 
-addBook.addEventListener('click', () => {
-    let book = document.createElement('div');
-    book.className = 'book'
-    book.textContent='Test';
-    content.appendChild(book)
+/* form.addEventListener('click', () => {
+    let form = document.createElement('form');
+    form.className = 'form'
+    form.textContent='Book Info';
+    content.appendChild(form)
+}); */
 
-});
+let library = [];
+/* constructor */
+function book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.info = () => {
+        return `${title},${author},${pages}`
+    } 
+    
+}
+let theHobbit = new book('Some title', 'me', '123')
+console.log(theHobbit.info())
+function addBook() {
+    let title = document.querySelector('.title').value;
+    let author = document.querySelector('.author').value;
+    let pages = document.querySelector('.pages').value;
+    let bookInfo = new book(title, author, pages)
+    if (title === '' || author === '') return;
+    library.push(bookInfo)
+    content.innerHTML = ''
+    for (i = 0; i < library.length; i++) {
+        /* display array into content */
+        let div = document.createElement('div') 
+        div.className = 'book'  
+        div.textContent = library[i].info()
+        content.appendChild(div)
+    }
+}
+
+
